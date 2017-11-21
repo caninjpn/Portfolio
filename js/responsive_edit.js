31737d5b0c81753535f6696e488148d4b816dbd5
DESKTOP-TB5ER2Q\Rinko_Desktop_JP <wong.cheuk.him.kevin@gmail.com>
2017-11-22 02:16:57 +0900

    minor adjustments

--- js/responsive_edit.js
+++ js/responsive_edit.js
@@ -1,13 +1,17 @@
 function checkWidth() {
     if ($(window).width() < 726) {
-        $('#glow_txt').addClass('show');
-        $('#glow_txt2').addClass('show');
-        $('#glow_txt3').addClass('show');
+        $('#proj_txt').addClass('show');
+        $('#proj_txt2').addClass('show');
+        $('#proj_txt3').addClass('show');
+        $('#proj_txt4').addClass('show');
     } else {
-        $('#glow_txt').removeClass('show');
-		$('#glow_txt2').removeClass('show');
-        $('#glow_txt3').removeClass('show');
+        $('#proj_txt').removeClass('show');
+		$('#proj_txt2').removeClass('show');
+        $('#proj_txt3').removeClass('show');
+        $('#proj_txt4').removeClass('show');
     }
 }
 
 $(window).resize(checkWidth);
+
+/*detect onclick and hide not clicked, show only $this */
