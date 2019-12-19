8bf943be5889db9f876c89566e9c55804795761e
Wong <kevin.wong@p32144.intra.rakuten.co.jp>
2019-12-19 10:55:09 +0900

    new menu

--- js/behavior.js
+++ js/behavior.js
@@ -1,20 +1,21 @@
 document.getElementById("menu_btn").addEventListener("click", showMenu, false);
-document.getElementById("link1").addEventListener("click", showMenu, false);
 
 function windowCheck() {
     var h = window.innerHeight;
     $('#intro').css("height", h);
     $('#nav').css("height", h);
     $('.row').css("min-height", h);
-    $('#link1').toggle();
 }
 
 function showMenu() {
-    $('#intro').toggleClass('menu1');
-    $('#Designer').toggleClass('menu2', 100, "easeInOutExpo");
-    $('#link1').toggle();
-    $('#title1').toggle();
-    $('#Maker').toggleClass('menu3', 100, "easeInOutExpo");
-    $('#Seeker').toggleClass('menu4', 100, "easeInOutExpo");
-    $('#Canadian').toggleClass('menu5', 100, "easeInOutExpo");
+    $('#Menu').animate({
+        top: 0
+    }, 1000, function (animatemenu) {});
+    
+}
+
+function animatemenu(){
+    $('.menu1').animate({
+        top: 0
+    }, 2000, function () {});
 }
